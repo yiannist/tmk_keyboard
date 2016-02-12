ead16368ce0623e43fb66e64f00542f6e69e234b
Loren Rogers <loren@lorentrogers.com>
2016-02-11 22:50:23 -0500

    Creates basic three layer setup for mouse

--- keyboard/atreus/keymap_common.h
+++ keyboard/atreus/keymap_common.h
@@ -77,17 +77,32 @@ extern const uint16_t fn_actions[];
 /*
  * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
  *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
- *       volup             reset      ||             F1    F2    F3   F12
+ * mouse volup             reset      ||             F1    F2    F3   F12
  *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
  */
 #define LAYER_TWO KEYMAP(KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP, \
-                         KC_UP, KC_F7, KC_F8, KC_F9, KC_F10, \
+                           KC_MS_U, KC_F7, KC_F8, KC_F9, KC_F10, \
                          KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, \
-                         KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11,          \
-                         KC_NO, KC__VOLUP, KC_NO, KC_NO, KC_FN3, \
-                         KC_NO, KC_F1, KC_F2, KC_F3, KC_F12,            \
+                           KC_MS_D, KC_F4, KC_F5, KC_F6, KC_F11,          \
+                         KC_FN3, KC__VOLUP, KC_NO, KC_NO, KC_FN4, \
+                           KC_NO, KC_F1, KC_F2, KC_F3, KC_F12,            \
                          KC_NO, KC__VOLDOWN, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, \
-                         KC_LALT, KC_SPC, KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE)
+                           KC_LALT, KC_SPC, KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE)
+
+/*
+ *                                    ||
+ *                                    ||     left   down   up   right
+ * mouse                              ||
+ *                                    ||
+ */
+#define MOUSE_LAYER KEYMAP(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+                             KC_NO, KC_MS_D, KC_NO, KC_NO, KC_NO, \
+                           KC_FN3, KC_NO, KC_NO, KC_NO, KC_NO, \
+                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
 
 enum function_id {
   BOOTLOADER,
