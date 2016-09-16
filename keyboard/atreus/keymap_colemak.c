5852a3a78602ecfbe693304bc4f7452f3f7d7903
Phil Hagelberg <phil@hagelb.org>
2016-09-16 09:03:01 +0700

    Fix colemak minus key.

--- keyboard/atreus/keymap_colemak.c
+++ keyboard/atreus/keymap_colemak.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O, \
          KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
          KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,     \
-         KC_SPC, KC_FN0, KC_QUOT, KC_LBRC, KC_ENT),                     \
+         KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT),                     \
   /* 1: punctuation and numbers */
   FN_ARROW_LAYER, \
   /* 2: arrows and function keys */
