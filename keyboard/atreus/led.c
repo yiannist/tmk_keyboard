678c570449fa3bc23405e293634282f0534fa239
Phil Hagelberg <phil@hagelb.org>
2017-09-24 22:01:31 -0700

    Disable caps lock LED.

--- keyboard/atreus/led.c
+++ keyboard/atreus/led.c
@@ -22,13 +22,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 void led_set(uint8_t usb_led)
 {
-    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-        // output low
-        DDRB |= (1<<2);
-        PORTB &= ~(1<<2);
-    } else {
-        // Hi-Z
-        DDRB &= ~(1<<2);
-        PORTB &= ~(1<<2);
-    }
 }
